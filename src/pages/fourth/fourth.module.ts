669e9d1ccf9b30aaa97c37365f5bbe8c9f83fa7c
sdhoju <sdhoju@go.olemiss.edu>
2018-10-01 17:45:49 -0500

    Get started commit

---
+++ src/pages/fourth/fourth.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { FourthPage } from './fourth';
+
+@NgModule({
+  declarations: [
+    FourthPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(FourthPage),
+  ],
+})
+export class FourthPageModule {}
