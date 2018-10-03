9f1b84d1494940a8892bbab613a60caae3230d8a
xavierb <xbuchana@go.olemiss.edu>
2018-10-03 17:11:51 -0500

    login page style change

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -8,20 +8,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
-import { Component } from '@angular/core';
-import { NavController, NavParams, IonicPage } from 'ionic-angular';
-
-@IonicPage();
+@IonicPage()
 @Component({
-	selector: 'page-login',
-	templateUrl: 'login.html',
+  selector: 'page-login',
+  templateUrl: 'login.html',
 })
-export class LoginPage{
-	constructor(public navCtrl: NavController, public navParams: NavParams, public IonicPage: IonicPage){
-	}
-	
-	login(){
-		this.navCtrl.push('HomePage');
-	}
+export class LoginPage {
+
+  constructor(private navCtrl: NavController, private navParams: NavParams) {
+  }
+
+  navigateToPage(pageName: string):void {
+    //condtion to check if the page is TabsPage or other pages
+    pageName =='TabsPage' ?  this.navCtrl.setRoot(pageName): this.navCtrl.push(pageName);
+  }
+
 }
-	
